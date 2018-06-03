36244bca1464bef5b405c93175ae4db616273a9f
Jesús León <jesus.rojas@almundo.com>
2018-06-03 20:15:44 -0300

    Remove HelloWorld example

--- src/main/java/com/mercadolibre/xmen/Application.java
+++ src/main/java/com/mercadolibre/xmen/Application.java
@@ -2,21 +2,13 @@ package com.mercadolibre.xmen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by jesus.leon on 12/05/18.
  */
 @SpringBootApplication
-@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello Spring Boot!";
     }
 }
